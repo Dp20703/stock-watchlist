516368a96d5ff0ac9797f3f3cd12aba1cb2d956f
dp <codewithdp2073@gmail.com>
2025-05-26 11:21:56 +0530

    Remove unused import from watchlist route

--- server/routes/watchlist.route.js
+++ server/routes/watchlist.route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const watchlistModel = require('../models/watchlist.model');
-const { use } = require('react');
 
 // Add to watchlist => [ /api/watchlist/add ]
 router.post('/add', async (req, res) => {
